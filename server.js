5e433673570f517b22401d36519edbb42c8f8afc
dennis <dennis@admod.com>
2020-11-20 15:24:14 +0530

    router creation

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
-console.log('May Node be with you')
+const express = require("express");
+const app = express();
+
+const port = 4000;
+const mongoose = require("mongoose");
+
+const employees = require("./model");
+const router = express.Router();
+
+//database connection
+var uri = "mongodb://localhost:27017/details";
+
+mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true });
+
+const connection = mongoose.connection;
+
+connection.once("open", function() {
+  console.log("MongoDB database connection established successfully at port 27017");
+});
+
+//start router- any link starting with '/' will be captured by the router
+app.use("/", router);
+
+app.listen(port, function() {
+  console.log("Server is running on Port: " + port);
+});
+
