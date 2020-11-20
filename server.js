3b04d56764dcf2eca5f1e8a5b05c8977eafb4f7b
dennis <dennis@admod.com>
2020-11-20 16:24:48 +0530

    post and fetch

--- server.js
+++ server.js
@@ -19,7 +19,49 @@ connection.once("open", function() {
 });
 
 //start router- any link starting with '/' will be captured by the router
+//
 app.use("/", router);
+router.route("/insertdata").post(function(req, res) {
+var data = [
+  {
+    name: "John",
+    age: 21,
+    location: "New York"
+  },
+  {
+    name: "Smith",
+    age: 27,
+    location: "Texas"
+  },
+  {
+    name: "Lisa",
+    age: 23,
+    location: "Chicago"
+  }
+];
+employees.insertMany(data, function(err, result) {
+  if (err) {
+    res.send(err);
+  } else {
+    res.send(result);
+  }
+});
+router.route("/fetchdata").get(function(req, res) {
+  employees.find({}, function(err, result) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+
+});
+
+
+
+
 
 app.listen(port, function() {
   console.log("Server is running on Port: " + port);
