e5d23fab6bc9c3a354ae8e66fd609c0f6d014d23
zuneishere <zuneishere@gmail.com>
2020-12-02 12:42:24 +0530

    second cloud build test

--- Docker-Distribution/server.js
+++ Docker-Distribution/server.js
@@ -16,8 +16,8 @@ const {
 
 //database connection
 //var uri = "mongodb://dennis:startup#85@host:27017/details";
-var uri = "mongodb://dennis:startup85@mongodb-release-headless:27017/details";
-//var uri = `${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/details`;
+//var uri = "mongodb://dennis:startup85@mongodb-release-headless:27017/details";
+var uri = `${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/details`;
 
 mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true });
 
